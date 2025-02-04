ce02847af586fbe790c1d6cf7233009ec46378c2
RoberthCepaya <86171309+RoberthCepaya@users.noreply.github.com>
2025-02-04 17:16:51 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,186 @@
+import { watchFile, unwatchFile } from 'fs' 
+import chalk from 'chalk'
+import { fileURLToPath } from 'url'
+import fs from 'fs'
+import cheerio from 'cheerio'
+import fetch from 'node-fetch'
+import axios from 'axios'
+import moment from 'moment-timezone' 
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+//BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
+//Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
+global.botNumberCode = '' //Ejemplo: +573218138672
+global.confirmCode = ''
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.owner = [
+  [ '50488198573', 'Deylin', true ],
+  [ '595975677765', 'Davidius', true ],
+  [ '5217641291269', 'José', true ]
+]; // <-- Corchete de cierre y coma añadidos
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.mods = ['50488198573', '50488198573', '', '', '']
+global.suittag = ['50488198573']
+global.prems = []
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.libreria = 'Baileys'
+global.baileys = 'V 6.7.9' 
+global.languaje = 'Español'
+global.vs = '2.1.5'
+global.vsJB = '5.0'
+global.nameqr = '࿋ོ༙𝑲𝒊𝒓𝒊𝒕𝒐-𝑩𝒐𝒕࿐'
+global.namebot = '᭄𝑲𝒊𝒓𝒊𝒕𝒐-𝑩𝒐𝒕࿐ཽ༵'
+global.sessions = 'Sessions'
+global.jadi = 'JadiBots' 
+global.kiritoJadibts = true
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.packsticker =`°.⎯⃘̶⎯̸⎯ܴ⎯̶᳞͇ࠝ⎯⃘̶⎯̸⎯ܴ⎯̶᳞͇ࠝ⎯⃘̶⎯̸.°\n✦ Bσƚ:\n✦ Pɾσριҽƚαɾισ:\n✦ Fҽƈԋα ԃҽ Cɾҽαƈιóɳ:\n✦ Hσɾα ԃҽ Cɾҽαƈιóɳ:\n♾━━━━━━━━`
+global.packsticker2 = `━━━━━━━━♾\n⪛✰ ᬊ𝐊𝐢𝐫𝐢𝐭𝐨-𝐁𝐨𝐭ᬊᬁ ✰⪜\n𝐃𝐞𝐲𝐥𝐢𝐧\n⇝ ${moment.tz('America/Los_Angeles').format('DD/MM/YY')}\n⇝ ${moment.tz('America/Los_Angeles').format('HH:mm:ss')} \n°.⎯⃘̶⎯̸⎯ܴ⎯̶᳞͇ࠝ⎯⃘̶⎯̸⎯ܴ⎯̶᳞͇ࠝ⎯⃘̶⎯̸.°\n\nѕτιϲκєя ϐγ: Kirito-Bot`
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.packname = `✰ཽ𝐊𝐢𝐫𝐢𝐭𝐨-𝐁𝐨𝐭ཉ`
+global.botname = 'ᬊᬁ𝐊𝐢𝐫𝐢𝐭𝐨-𝐁𝐨𝐭࿐'
+global.wm = '๖ۣ•҉𝐊𝐢𝐫𝐢𝐭𝐨-𝐁𝐨𝐭᭄'
+global.author = 'Made By 𝑫𝒆𝒚𝒍𝒊𝒏'
+global.dev = '© ⍴᥆ᥕᥱrᥱძ ᑲᥡ 𝑫𝒆𝒚𝒍𝒊𝒏'
+global.textbot = '𝑲𝒊𝒓𝒊𝒕𝒐-𝑩𝒐𝒕 • Powered By 𝑫𝒆𝒚𝒍𝒊𝒏'
+
+global.moneda = 'Yenes'
+global.welcom1 = 'Edita Con #setwelcome'
+global.welcom2 = 'Edita Con #setbye'
+global.banner = 'https://tinyurl.com/25md9gsv'
+global.catalogo = 'https://qu.ax/MuAQA.jpg'
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.imagen1 = fs.readFileSync('./src/menus/Menu2.jpg');
+global.imagen2 = fs.readFileSync('./src/anime.jpg');
+global.imagen3 = fs.readFileSync('./src/menus/Menu3.jpg');
+global.imagen4 = fs.readFileSync('./src/menus/Menu.jpg');
+global.imagen5 = fs.readFileSync('./src/+18.jpg');
+global.imagen6 = fs.readFileSync('./src/menus/Menu3.jpg');
+global.imagen7 = fs.readFileSync('./src/menus/Menu5.jpg');
+global.imagen8 = fs.readFileSync('./src/menus/Menu4.jpg')
+global.imagen9 = fs.readFileSync('./src/menu_en.png')
+global.imagen10 = fs.readFileSync('./src/nuevobot.jpg')
+global.miniurl = fs.readFileSync('./src/Grupo.jpg');
+global.logo2 = fs.readFileSync('./src/logo2.jpg')
+global.logo3 = fs.readFileSync('./src/logo3.jpg')
+global.catalogo = fs.readFileSync('./src/logo6.png')
+global.logo4 = fs.readFileSync('./src/logo4.jpg')
+global.logo5 = fs.readFileSync('./src/logo5.jpg')
+global.logo7 = fs.readFileSync('./src/Logo7.png')
+global.logo8 = fs.readFileSync('./src/Logo8.jpg')
+global.rule = fs.readFileSync('./src/rule.jpg')
+
+global.photoSity = [imagen8, imagen1, imagen4, imagen6]
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.gp4 = 'https://chat.whatsapp.com/F8KwM3rVqkS9HhR5msoRqQ'
+global.gp1 = 'https://chat.whatsapp.com/F8KwM3rVqkS9HhR5msoRqQ' 
+global.gp2 = 'https://chat.whatsapp.com/F8KwM3rVqkS9HhR5msoRqQ'
+global.comunidad1 = 'https://chat.whatsapp.com/LmJUVlnRwzJF6GM2KzBIXz'
+global.channel = 'https://whatsapp.com/channel/0029VawF8fBBvvsktcInIz3m'
+global.channel2 = 'https://whatsapp.com/channel/0029VayQwPsFnSzESZJ9Us3z'
+global.md = 'https://github.com/deylinqff/Kirito_Bot'
+global.correo = 'thekingdestroy507@gmail.com'
+global.cn ='https://whatsapp.com/channel/0029VawF8fBBvvsktcInIz3m';
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "120363365864927734@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: '𝑲𝒊𝒓𝒊𝒕𝒐-𝑩𝒐𝒕 ☄︎︎', orderTitle: 'Bang', thumbnail: catalogo, sellerJid: '0@s.whatsapp.net'}}}
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.ch = {
+ch1: '120363365444927738@newsletter',
+ch2: '120363365444927738@newsletter',
+}
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.cheerio = cheerio
+global.fs = fs
+global.fetch = fetch
+global.axios = axios
+global.moment = moment   
+
+global.rpg = {
+  emoticon(string) {
+    string = string.toLowerCase();
+    const emot = {
+      level: '🌟 Nivel',
+      coin: '💸 Coin',
+      exp: '✨ Experiencia',
+      bank: '🏦 Banco',
+      diamond: '💎 Diamante',
+      health: '❤️ Salud',
+      kyubi: '🌀 Magia',
+      joincount: '💰 Token',
+      emerald: '♦️ Esmeralda',
+      stamina: '⚡ Energía',
+      role: '⚜️ Rango',
+      premium: '🎟️ Premium',
+      pointxp: '📧 Puntos Exp',
+      gold: '👑 Oro',
+      iron: '⛓️ Hierro',
+      coal: '🌑 Carbón',
+      stone: '🪨 Piedra',
+      potion: '🥤 Poción',
+    };
+    const results = Object.keys(emot).map((v) => [v, new RegExp(v, 'gi')]).filter((v) => v[1].test(string));
+    if (!results.length) return '';
+    else return emot[results[0][0]];
+  }};
+global.rpgg = { 
+  emoticon(string) {
+    string = string.toLowerCase();
+    const emott = {
+      level: '🌟',
+      coin: '💸',
+      exp: '✨',
+      bank: '🏦',
+      diamond: '💎',
+      health: '❤️',
+      kyubi: '🌀',
+      joincount: '💰',
+      emerald: '♦️',
+      stamina: '⚡',
+      role: '⚜️',
+      premium: '🎟️',
+      pointxp: '📧',
+      gold: '👑',
+      iron: '⛓️',
+      coal: '🌑',
+      stone: '🪨',
+      potion: '🥤',
+    };
+    const results = Object.keys(emott).map((v) => [v, new RegExp(v, 'gi')]).filter((v) => v[1].test(string));
+    if (!results.length) return '';
+    else return emott[results[0][0]];
+  }};  
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.multiplier = 69
+global.maxwarn = '3'
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+let file = fileURLToPath(import.meta.url)
+watchFile(file, () => {
+  unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  import(`${file}?update=${Date.now()}`)
+})
